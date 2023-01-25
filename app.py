ec55f8f4fe4327eb2253b2a7abeb6364fd275022
Anton <5991anton@gmail.com>
2023-01-25 10:37:27 +0300

    rRussian translate

--- app.py
+++ app.py
@@ -33,31 +33,31 @@ def get_key(val,my_dict):
 
 
 def main():
-	"""News Classifier"""
-	st.title("News Classifier")
+	"""Классификация новостей"""
+	st.title("Классификация новостей")
 	# st.subheader("ML App with Streamlit")
 	html_temp = """
 	<div style="background-color:blue;padding:10px">
-	<h1 style="color:white;text-align:center;">Streamlit ML App </h1>
+	<h1 style="color:white;text-align:center;">Первичный анализ новостей с помощью бибилотеки spaCy </h1>
 	</div>
 
 	"""
 	st.markdown(html_temp,unsafe_allow_html=True)
 
-	activity = ['Prediction','NLP']
-	choice = st.sidebar.selectbox("Select Activity",activity)
+	activity = ['Прогноз','NLP(обработка естественного языка)']
+	choice = st.sidebar.selectbox("Выберите операцию",activity)
 
 
-	if choice == 'Prediction':
-		st.info("Prediction with ML")
+	if choice == 'Прогноз':
+		st.info("Предсказание типа новостей")
 
-		news_text = st.text_area("Enter News Here","Type Here")
+		news_text = st.text_area("Введите текст на английском языке","поле ввода")
 		all_ml_models = ["LR","RFOREST","NB","DECISION_TREE"]
-		model_choice = st.selectbox("Select Model",all_ml_models)
+		model_choice = st.selectbox("Выберите модель",all_ml_models)
 
-		prediction_labels = {'business': 0,'tech': 1,'sport': 2,'health': 3,'politics': 4,'entertainment': 5}
-		if st.button("Classify"):
-			st.text("Original Text::\n{}".format(news_text))
+		prediction_labels = {'предпринимательство': 0,'технологии': 1,'спорт': 2,'здоровье': 3,'политика': 4,'развлечения': 5}
+		if st.button("Классификация новостей"):
+			st.text("Исходный текст::\n{}".format(news_text))
 			vect_text = news_cv.transform([news_text]).toarray()
 			if model_choice == 'LR':
 				predictor = load_prediction_models("models/newsclassifier_Logit_model.pkl")
@@ -77,39 +77,39 @@ def main():
 				# st.write(prediction)
 
 			final_result = get_key(prediction,prediction_labels)
-			st.success("News Categorized as:: {}".format(final_result))
+			st.success("Новости отнесены к категории:: {}".format(final_result))
 
 	if choice == 'NLP':
-		st.info("Natural Language Processing of Text")
-		raw_text = st.text_area("Enter News Here","Type Here")
-		nlp_task = ["Tokenization","Lemmatization","NER","POS Tags"]
+		st.info("Обработка естественного языка (новости)")
+		raw_text = st.text_area("Введите текст на английском языке","поле ввода")
+		nlp_task = ["Токенизация","Лемматизация","NER","Теги частей речи"]
 		task_choice = st.selectbox("Choose NLP Task",nlp_task)
-		if st.button("Analyze"):
-			st.info("Original Text::\n{}".format(raw_text))
+		if st.button("Проанализировать"):
+			st.info("Исходные данные::\n{}".format(raw_text))
 
 			docx = nlp(raw_text)
-			if task_choice == 'Tokenization':
+			if task_choice == 'Токенизация':
 				result = [token.text for token in docx ]
-			elif task_choice == 'Lemmatization':
+			elif task_choice == 'Лемматизация':
 				result = ["'Token':{},'Lemma':{}".format(token.text,token.lemma_) for token in docx]
 			elif task_choice == 'NER':
 				result = [(entity.text,entity.label_)for entity in docx.ents]
-			elif task_choice == 'POS Tags':
-				result = ["'Token':{},'POS':{},'Dependency':{}".format(word.text,word.tag_,word.dep_) for word in docx]
+			elif task_choice == 'Теги частей речи':
+				result = ["'Токен':{},'Часть речи':{},'Зависимость':{}".format(word.text,word.tag_,word.dep_) for word in docx]
 
 			st.json(result)
 
-		if st.button("Tabulize"):
+		if st.button("Таблица"):
 			docx = nlp(raw_text)
 			c_tokens = [token.text for token in docx ]
 			c_lemma = [token.lemma_ for token in docx ]
 			c_pos = [token.pos_ for token in docx ]
 
-			new_df = pd.DataFrame(zip(c_tokens,c_lemma,c_pos),columns=['Tokens','Lemma','POS'])
+			new_df = pd.DataFrame(zip(c_tokens,c_lemma,c_pos),columns=['Токены','Лемма','Часть речи'])
 			st.dataframe(new_df)
 
 
-		if st.checkbox("WordCloud"):
+		if st.checkbox("Облако слов"):
 			c_text = raw_text
 			wordcloud = WordCloud().generate(c_text)
 			plt.imshow(wordcloud,interpolation='bilinear')
@@ -124,7 +124,7 @@ def main():
 
 
 
-	st.sidebar.subheader("About")
+	st.sidebar.subheader("Исполнители: Зайцев Александр Васильевич, Чурилов Алексей Александрович, Зайцев Антон Александрович")
 
 
 

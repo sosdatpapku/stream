7d5cd69c1a9cbc8d0245c384f300bb23081579a7
sosdatpapku <114177169+sosdatpapku@users.noreply.github.com>
2023-03-25 17:46:57 +0400

    Update locust_test.py

--- locust_test.py
+++ locust_test.py
@@ -3,11 +3,12 @@ from locust import HttpUser, task, between
 # Locust используется для тестирования производительности работы web-приложения на облаке Streamlit
 class AppUser(HttpUser):
     wait_time = between(5, 10)
-    
-    
+
     @task
     def home_page(self):
         self.client.get('/')
+        
+        
 """
 Locust используется для тестирования производительности работы
 web-приложения на облаке Streamlit.

1518f900dc7286c42634e4e71a8b09814cee8026
sosdatpapku <metall_hair@mail.ru>
2023-04-15 14:05:54 +0400

    Update locust_test.py

--- locust_test.py
+++ locust_test.py
@@ -1,27 +1,22 @@
 from locust import HttpUser, task, between
 
 
-# Locust используется для тестирования производительности работы web-приложения на облаке Streamlit
+# Класс AppUser имитирует выполнение HTTP-запросов на указанной странице используя импортированный класс HttpUser
 class AppUser(HttpUser):
-    wait_time = between(5, 10)
+    wait_time = between(5, 10) # интервал выполнения HTTP-запросов на странице (в секундах)
 
-    @task
-    def home_page(self):
+    @task # В task описываются действия при каждом HTTP-запросе
+    def get_page(self): # Функция внешнего запроса страницы
         self.client.get('/')
 
 
 """
-Locust используется для тестирования производительности работы
-web-приложения на облаке Streamlit.
-Имитируем параллельное использование
-приложения несколькими пользователями
+Алгоритм воспроизведения теста
 
-
-USAGE
-
-streamlit run app_tsch.py
+1) Запускаем наше приложение на облаке streamlit через команду
+streamlit run lite_ap.py
+2) Запускаем веб-интерфейс locust для нашего теста через команду
 locust -f locust_test.py
-And go to the link http://localhost:8089
-
-Alex Zaitsev, Alex Churilov
+3) Переходим по ссылке http://localhost:8089
+4) В интерфейсе указываем адрес страницы веб приложения, которое требуется протестировать
 """

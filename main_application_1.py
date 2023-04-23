491a02429204cb0184b4a21be2242e0cb3b88e9c
PetrGavrilin <gapeal@mail.ru>
2023-04-23 11:09:46 +0300

    small changes

--- main_application_1.py
+++ main_application_1.py
@@ -1,14 +1,9 @@
-import vk  # импорт специализированной библиотеки для парсинга текста
 import lite_ap # импорт скрипта lite_ap.py
-import os  # для импорта токена
-import vk_token  # для импорта токена
-import vk  # импорт специализированной библиотеки для парсинга текста
-
 
 def main():
     
     try:
-        api_for = vk.API(access_token=token)  # адрес токена вк
+        api_for = 'vk.API(access_token=token)'  # адрес токена вк
     except:  
         api_for = None
 
